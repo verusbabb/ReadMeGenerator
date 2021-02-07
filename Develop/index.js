2f6477139918badac4a9ea4385fea7371e3c0619
Steve Babb <steve.babb@outlook.com>
2021-02-07 10:46:22 -0600

    Questions created

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,26 @@
-Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project?', 
+    'What is a brief description of your project?',
+    'What are the installation instructions?',
+    'Please specify any usage information...',
+    'Please provide any contribution guidelines...',
+    'Please provide any necessary test instructions...',
+    'Please choose a desired license for your project...',
+    'Please enter your GitHub username...',
+    'Please enter your email address...'
+];
 
+console.log(questions[0]);
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
